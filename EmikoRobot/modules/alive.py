774f9eb24edeb4544000019c2c51450c45784dcb
THEURANIUM92 <101351302+THEURANIUM92@users.noreply.github.com>
2022-03-28 18:01:59 +0530

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -14,11 +14,11 @@ PHOTO = "https://telegra.ph/file/efe1fe3be3414134a448e.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
-  TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [URANIUM](https://t.me/THE_URANIUM)** \n\n"
-  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
-  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
+  TEXT += "♨️ **I'm Working Properly** \n\n"
+  TEXT += f"♨️ **Mʏ Mᴀsᴛᴇʀ : [URANIUM](https://t.me/THE_URANIUM)** \n\n"
+  TEXT += f"♨️ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ :** `{telever}` \n\n"
+  TEXT += f"♨️ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+  TEXT += f"♨️ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
+  TEXT += "**Tʜᴀɴᴋs Fᴏʀ Aᴅᴅɪɴɢ Mᴇ Hᴇʀᴇ ❤️**"
   BUTTON = [[Button.url("Help", "https://t.me/1URANIUM_MUSIC_bot?start=help"), Button.url("Support", "https://t.me/URANIUM_MUSIC_SUPPORT")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
