e66514869c65f0e44c1b3d12f1bbbf575e36361d
THEURANIUM92 <THEURANIUM92@users.noreply.github.com>
2022-03-28 12:46:48 +0000

    Automated code formatting
    Signed-off-by: kennedy-ex <kennedy-ex@users.noreply.github.com>

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -11,14 +11,20 @@ from EmikoRobot import telethn as tbot
 
 PHOTO = "https://telegra.ph/file/efe1fe3be3414134a448e.jpg"
 
+
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
-  TEXT += "♨️ **I'm Working Properly** \n\n"
-  TEXT += f"♨️ **Mʏ Mᴀsᴛᴇʀ : [ᴜʀᴀɴɪᴜᴍ](https://t.me/THE_URANIUM)** \n\n"
-  TEXT += f"♨️ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ :** `{telever}` \n\n"
-  TEXT += f"♨️ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
-  TEXT += f"♨️ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
-  TEXT += "**Tʜᴀɴᴋs Fᴏʀ Aᴅᴅɪɴɢ Mᴇ Hᴇʀᴇ ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/1URANIUM_MUSIC_bot?start=help"), Button.url("Support", "https://t.me/URANIUM_MUSIC_SUPPORT")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
+    TEXT += "♨️ **I'm Working Properly** \n\n"
+    TEXT += f"♨️ **Mʏ Mᴀsᴛᴇʀ : [ᴜʀᴀɴɪᴜᴍ](https://t.me/THE_URANIUM)** \n\n"
+    TEXT += f"♨️ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ :** `{telever}` \n\n"
+    TEXT += f"♨️ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+    TEXT += f"♨️ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
+    TEXT += "**Tʜᴀɴᴋs Fᴏʀ Aᴅᴅɪɴɢ Mᴇ Hᴇʀᴇ ❤️**"
+    BUTTON = [
+        [
+            Button.url("Help", "https://t.me/1URANIUM_MUSIC_bot?start=help"),
+            Button.url("Support", "https://t.me/URANIUM_MUSIC_SUPPORT"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
